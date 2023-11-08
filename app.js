9cb10d2cb651dc790762ecc5f890aa8483f5e60a
Nicholas Harper <harpergnicholas@gmail.com>
2023-11-08 11:55:59 +1000

    Fixed number formatting and updated button handling (cost)

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
+ $(document).ready(function(){
+
 //Variables
+//Main currency
 let fish = {
-    count : 0,
+    count : 1,
     value : 1,
-    multi : 1,
+    multi : 10000,
     lifetime: 0
 };
-let gameStage = 0;
 fishUpgrade = [
     {
         stageReq: 0,
@@ -29,9 +30,18 @@ fishUpgrade = [
         countIncrease : 10000,
         level : 0
     }
-]
+];
+featureUpgrade = [
+    {
+        desc: 0,
+        cost : 10,
+        costMulti : 2,
+        countIncrease : 1,
+        level : 0
+    }
+];
 
-let upgradesList = [{type:"fish", data:fishUpgrade}]
+let upgradesList = [{type:"fish", data:fishUpgrade}];//,{type:"feature", data:featureUpgrade}]
 
 // Function to collect fish
 $("#collectFish").click(function() {
@@ -67,7 +77,7 @@ function addFishUpgradeButton(index,upgrade){
     newButton.type = "fish";
     newButton.setAttribute("index", index);
     newButton.className = "upgradeButton";
-    newButton.textContent = "Increase Fish Collection (",upgrade.countIncrease,")";
+    newButton.textContent = "Fish Collection + ",upgrade.countIncrease;
 
     // Create a paragraph element for the cost information
     const costParagraph = document.createElement("p");
@@ -88,31 +98,25 @@ function addFishUpgradeButton(index,upgrade){
     parentElement.appendChild(newButton);
 }
 
-function gameStageCheck(){
-
-}
-
 // Function to process the purchase of an upgrade
 function upgradeBought(data){
     // Get the index and type of the clicked button
     let index = data.getAttribute('index');
-
     // Iterate through the upgradesList to find the matching upgrade
-    for (let i=0; i < upgradesList.length; i++){
         // Check if the type of the clicked button matches the current upgrade type
-        if (data.getAttribute('type') == upgradesList[i].type){
+        if (data.getAttribute('type') == "fish"){
+            let fishupgradeBttn = upgradesList[0].data[index];
             // Check if the player has enough fish to purchase the upgrade
-            if (fish.count >= upgradesList[i].data[index].cost) {
-                fish.count -= upgradesList[i].data[index].cost;
-                fish.value += upgradesList[i].data[index].countIncrease;
-                upgradesList[i].data[index].level += 1;
+            if (fish.count >= fishupgradeBttn.cost) {
+                fish.count -= fishupgradeBttn.cost;
+                fish.value += fishupgradeBttn.countIncrease;
+                fishupgradeBttn.level += 1;
                 // Update the cost of the upgrade and display it in the button
-                upgradesList[i].data[index].cost *= upgradesList[i].data[index].costMulti;
-                data.querySelector(".cost").innerHTML = formatNumber(upgradesList[i].data[index].cost);
-                data.querySelector(".owned").innerHTML = formatNumber(upgradesList[i].data[index].level);
+                fishupgradeBttn.cost *= fishupgradeBttn.costMulti;
+                data.querySelector(".cost").innerHTML = formatNumber(fishupgradeBttn.cost);
+                data.querySelector(".owned").innerHTML = formatNumber(fishupgradeBttn.level);
             }
         }
-    }
     //Update buttons and stats on the page
     updateDisplay();
 }
@@ -121,11 +125,17 @@ function upgradeBought(data){
 function buttonCheck(button){
     document.querySelectorAll('.upgradeButton').forEach(function (button){
         // Disable the button if the player doesn't have enough fish to buy the upgrade
-            if (fish.count < button.querySelector(".cost").innerHTML) {
-                button.disabled = true;
-              } else {
-                button.disabled = false;
-              }
+            // console.log(button);
+            // console.log(upgradesList[0].data[button.getAttribute("index")]);
+            if (button.getAttribute("type") == "fish"){
+                let cost = upgradesList[0].data[button.getAttribute("index")].cost;
+                if (fish.count < cost) {
+                    button.disabled = true;
+                  } else {
+                    button.disabled = false;
+                  }
+            }
+            
     });
 }
 
@@ -140,13 +150,13 @@ function updateDisplay() {
     $("#fishCount").html(formatNumber(fish.count));
     $("#fishValue").html(formatNumber(fish.value));
     buttonCheck();
-    gameStageCheck();
 }
 
 // Function to initialize the game
 function initializeGame() {
     updateDisplay();
     $("#upgrades").css("display", "block");
+    $("#shopBttn").disabled = true;
 }
 
 // Initialize the game
@@ -154,17 +164,18 @@ initializeGame();
 
 
 
-
+//Format the number as 1k, 1m then 1eX
 function formatNumber(number) {
-    if (number < 1000) {
-        return number.toString(); // No formatting needed for numbers less than 1000
-    } else if (number < 1000000) {
-        return (number / 1000).toFixed(2) + 'k'; // Format as 'X.XXk' for thousands
-    } else {
-        return (number / 1000000).toFixed(2) + 'm'; // Format as 'X.XXm' for millions
-    }
+        if (number < 1000) {
+            return number.toString(); // No formatting needed for numbers less than 1000
+        } else if (number < 1000000) {
+            return (number / 1000).toFixed(2) + 'k'; // Format as 'X.XXk' for thousands
+        } else if (number < 1000000000){
+            return (number / 1000000).toFixed(2) + 'm'; // Format as 'X.XXm' for millions
+        } else {
+            return new Decimal(number).toStringWithDecimalPlaces(2);
+        }
 }
-
-});
+ });
 
 
